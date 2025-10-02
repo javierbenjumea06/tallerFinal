5454c74527d915f53e241588b600332f9423f81c
Tomas Vega <tomas.vega.9012@gmail.com>
2025-10-02 18:36:10 -0500

    se añadio el export

--- menu.js
+++ menu.js
@@ -1,4 +1,6 @@
-const menuF = prompt(`ACCEDE A UNO DE LOS MENUS: 
+
+export function menu () {
+    const menuF = prompt(`ACCEDE A UNO DE LOS MENUS: 
         1. Registrar usuarios. 
         2. Registrar libros. 
         3. Prestar un libro.
@@ -6,18 +8,12 @@ const menuF = prompt(`ACCEDE A UNO DE LOS MENUS:
         5. Listar:
         6. Salir.`)
 
-if (menuF == 1) {
-    const menu1 = (`ELIGE UNA DE LAS FUNCIONES: 
-    1. Agregar un producto.
-    2. Listar los productos.
-    3. Buscar productos por nombre o categoria.`)
-
     if (menu1 == 1){
         Registrarusuarios()
     } else if (menu1 == 2){
         Registrarlibros()
     } else if (menu1 == 3){
-        Prestarunlibro()
+        prestarunlibro()       
     } else if (menu1 == 4){
         Devolverunlibro()
     } else if (menu1 ==  5){
