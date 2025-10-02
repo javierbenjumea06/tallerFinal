a20b5f89abe8b1c75ed36fe11a4e728c88a2973f
javierbenjumea06 <javier.benjumea@upb.edu.co>
2025-10-01 20:50:42 -0500

    Función para prestar libros lista

--- prestamos.js
+++ prestamos.js
@@ -3,3 +3,39 @@ Solo si está disponible.
 Cambiar el estado a prestado.
 Devolver un libro:
 Cambiar el estado a disponible.*/
+
+let libros = [{
+    "titulo":"Cien Años de Soledad",
+    "autor":"Tomas Vega",
+    "estado":"disponible",
+}, 
+{
+    "titulo":"Harry Potter: Programación Magica",
+    "autor":"Javier Benjumea",
+    "estado":"prestado",
+},
+{
+    "titulo":"Battle Royale",
+    "autor":"Fahim Florez",
+    "estado":"disponible",
+}]
+
+const libroParaPrestar = "Cien Años de Soledad"
+
+const prestarLibro = (libros, libroParaPrestar) =>
+{
+console.log("Por favor introduce el titulo del libro que quieres pedir prestado.")
+const busqueda = libros.find(a => a.titulo.includes(libroParaPrestar))
+if (!busqueda) {
+    console.log("No se encontró el libro.")
+} else if (busqueda.estado != "disponible") {
+    console.log("Este libro no se encuentra disponible en el momento")
+} else {
+    console.log("Puedes llevarte el libro.")
+    busqueda.estado = "prestado"
+}
+}
+
+
+
+
