1d31ca7b5301ae6c98aa3af613acf39d7ec33849
Tomas Vega <tomas.vega.9012@gmail.com>
2025-10-01 20:42:06 -0500

    ya fue

--- archivo usuarios.js
+++ archivo usuarios.js
@@ -5,14 +5,15 @@ const usuarios = [
   { nombre: 'Pedro Alvarez', email: 'pna@email.com' }
 ];
 
-const nombre = ""
-const gmail = ""
+const nombre = "Ernesto Orozco"
+const email = "ernesto.orozco@gmail.com"
 function registrarUsuario(nombre, email) {
-  if (!nombre || !email) {
+    if (!nombre || !email) {
     console.log('Error: Debes proporcionar nombre y correo electrónico.');
-    return;
-
   }
     usuarios.push({ nombre, email });
-   console.log(`Usuario registrado: ${nombre} - ${email}`);
+    console.log(`Usuario registrado: ${nombre} - ${email}`);
 }
+
+registrarUsuario(nombre, email)
+console.log(usuarios)
